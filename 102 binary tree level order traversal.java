f23fa3b2ce6e3225c66a2853a12b30ffcaf2589e
zachjcomer <zachjcomer@gmail.com>
2022-11-24 13:47:27 -0500

    TreeNode class

--- 102 binary tree level order traversal.java
+++ 102 binary tree level order traversal.java
@@ -1,27 +1,46 @@
-public List<List<Integer>> levelOrder(TreeNode root) {
-    List<List<Integer>> list = new LinkedList<>();
-    Queue<TreeNode> queue = new LinkedList<>();
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
-    if (root != null) queue.add(root);
-    int lvl = 0;
-    
-    while (!queue.isEmpty()) {
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
 
-        int len = queue.size();
-        List<Integer> listLevel = new LinkedList<>();
+        if (root != null) queue.add(root);
+        int lvl = 0;
+        
+        while (!queue.isEmpty()) {
 
-        for (int i = 0; i < len; i++) {
-            TreeNode cur = queue.poll();
+            int len = queue.size();
+            List<Integer> listLevel = new LinkedList<>();
 
-            listLevel.add(cur.val);
+            for (int i = 0; i < len; i++) {
+                TreeNode cur = queue.poll();
 
-            if (cur.left != null) queue.add(cur.left);
-            if (cur.right != null) queue.add(cur.right);
+                listLevel.add(cur.val);
 
+                if (cur.left != null) queue.add(cur.left);
+                if (cur.right != null) queue.add(cur.right);
+
+            }
+
+            list.add(listLevel);
+            lvl++;
         }
 
-        list.add(listLevel);
-        lvl++;
+        return list;
     }
-    return list;
 }
