01b810150540fc9de6880649aad1b1edf83ddc32
zachjcomer <zachjcomer@gmail.com>
2022-11-24 14:38:05 -0500

    Update 217 contains duplicate.java

--- hashing/217 contains duplicate.java
+++ hashing/217 contains duplicate.java
@@ -2,6 +2,9 @@
 https://leetcode.com/problems/contains-duplicate/description/
 
 duplicate -> already in hash set -> return true
+
+time: O(n) -> potentially add/lookup every element once
+space: O(n) -> size of set
 */
 
 class Solution {
