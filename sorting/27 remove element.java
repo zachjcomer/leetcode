a4100f8878500ec2933f75711564e7be57ee9393
zachjcomer <zachjcomer@gmail.com>
2022-12-19 12:16:11 -0500

    Update 27 remove element.java

--- sorting/27 remove element.java
+++ sorting/27 remove element.java
@@ -1,10 +1,10 @@
 /*
 https://leetcode.com/problems/remove-element/description/
 
-a quickselect -> if nums[i] != val, swap with index j, increment j -> otherwise go to next i -> puts all nums[i] != val on left, all nums[i] == val on right
+a quickselect -> if nums[i] != val, copy to index j, increment j -> otherwise go to next i -> puts all nums[i] != val on left, all nums[i] == val on right
 
 time: O(n) -> single pass
-space: O(1) -> quickselect in place O(1) -> pointer to swap index O(1)
+space: O(1) -> quickselect in place O(1) -> pointer to copy index O(1)
 */
 
 class Solution {
@@ -13,9 +13,7 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+                nums[j] = nums[i];
                 j++;
             }
         }
