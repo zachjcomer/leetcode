9b22daf35951a898672846eba4608c1a08dfa717
zachjcomer <zachjcomer@gmail.com>
2023-01-08 17:46:12 -0500

    Update 149 max points on a line.java

--- math/149 max points on a line.java
+++ math/149 max points on a line.java
@@ -7,41 +7,33 @@ pro: dont have to worry about slope-intercept form
 con: check each pair twice (a -> b and b -> a), getLine() will be Float.POSITIVE_INFINITY or Float.NEGATIVE_INFINITY if pt2[0] - pt1[0] is Float.NaN so must specially check those cases
 
 time: O(n^2) -> get slope for all points pairs O(n^2) -> count point-slope occurence O(1)
-space: O(n^2) -> map each point O(n) -> to a map of slopes w/ other points O(n)
+space: O(n) -> map each point O(1) -> to a map of slopes w/ other points O(n)
 */
 
 class Solution {
     public int maxPoints(int[][] points) {
-        int max = 1;
+        int out = 1;
+        if (points.length == 1) {
+            return out;
+        }
+        
+        if (points.length > 1) {
+            for (int i = 0; i < points.length; i++) {
+                Map<Double, Integer> map = new HashMap<>();
+                int[] pt1 = points[i];
 
-        Map<Integer, Map<Float, Integer>> map = new HashMap<>();
-        for (int i = 0; i < points.length; i++) {
-            map.put(i, new HashMap<Float, Integer>());
-            int[] pt1 = points[i];
-
-            for (int j = 0; j < points.length && j != i; j++) {
-                int[] pt2 = points[j];
-                
-                float s;
-                if (pt1[0] != pt2[0]) {
-                    s = getLine(pt1, pt2);
-                } else {
-                    s = Float.POSITIVE_INFINITY;
+                for (int j = 0; j < points.length; j++) {
+                    if (i != j) {
+                        int[] pt2 = points[j];
+                        
+                        double s = Math.atan2(pt2[1] - pt1[1], pt2[0] - pt1[0]);
+                        map.merge(s, 1, Integer::sum);
+                    } 
                 }
-                
-                Map<Float, Integer> pointMap = map.get(i);
-                pointMap.put(s, pointMap.getOrDefault(s, 0) + 1);
-
-                max = Math.max(max, pointMap.get(s) + 1);
+                out = Math.max(out, Collections.max(map.values()) + 1);
             }
         }
-
-        return max;
-    }
-
-    private float getLine(int[] pt1, int[] pt2) {
-        if (pt1[0] > pt2[0]) return getLine(pt2, pt1);
-
-        return (pt2[1] - pt1[1]) / ((float) pt2[0] - pt1[0]);
+        
+        return out;
     }
 }
