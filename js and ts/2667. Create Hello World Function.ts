ca5d89eeaa49c0c03e20413ff3edddbb3e09d435
zachjcomer <zachjcomer@gmail.com>
2023-05-17 19:52:06 -0400

    Create 2667. Create Hello World Function.ts

---
+++ js and ts/2667. Create Hello World Function.ts
@@ -0,0 +1,9 @@
+/*
+https://leetcode.com/problems/create-hello-world-function/submissions/?utm_campaign=PostD1&utm_medium=Post&utm_source=Post&gio_link_id=QPDw0kJR
+
+return an arrow func that returns "Hello World"
+*/
+
+function createHelloWorld() {
+    return () => "Hello World";
+};
