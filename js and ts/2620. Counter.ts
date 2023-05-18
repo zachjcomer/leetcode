963dc3e389371c86f49fb43de9f554d613a23d5f
zachjcomer <zachjcomer@gmail.com>
2023-05-17 20:20:48 -0400

    Create 2620. Counter.ts

---
+++ js and ts/2620. Counter.ts
@@ -0,0 +1,7 @@
+/*
+https://leetcode.com/problems/counter/description/?utm_campaign=PostD2&utm_medium=Post&utm_source=Post&gio_link_id=xogkVqBo
+
+return a function that returns n and then increments
+*/
+
+const createCounter = (n: number) => () => n++;
